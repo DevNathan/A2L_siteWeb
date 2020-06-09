ee01478effc734a89d4c62a8c2610568ee265870
Nathan Stchepinsky <nathanstchepinsky@gmail.com>
2020-06-09 12:17:08 +0200

    END OF PROJECT
    Dernière mise a jour et fermeture officielle du site
    
    Suite au non renouvellement du contrats passé entre l'A2L et le gestionnaire de serveurs, les technologies proposées par l'A2L en avril 2019 cesserons de fonctionner.
    Une copie de la base de donnée a été sauvegardé en local.
    
    Tous les repository restent disponibles.
    
    Les codes sources du serveur, de l'application iOS et du site web restent disponibles pour tous et peuvent être à l'origine d'une renaissance ou d'une revirement de projet.
    
    Avec beaucoup d'émotion, j'annonce officiellement, le projet du développeur Stchepinsky Nathan, terminé et clos.
    
    Je reste à la disposition de tout développeur intéressé, via
    - mon adresse mail : nathanstchepinsky@protonmail.com
    - mon site web : nathanstchepinsky--nathans1.repl.co
    
    Ce fus un réel plaisir.
    
    Merci à l'A2L.
    
    STCHEPINSKY Nathan, développeur.
    
    ####################### END OF PROJECT #######################

--- source/Class/EtatSite/etat_site.php
+++ source/Class/EtatSite/etat_site.php
@@ -3,11 +3,13 @@ Class EtatSite {
     function getMessage(){
         //$message ="Votre développeur préféré continue de construire ce site ... Quelques bugs peuvent donc encore se cacher, veuillez m'en excuser";
         //$message ="Votre développeur préféré est en plein travail de Une maintenance sur ce site. Veuillez excuser, les potentiels bugs et l'ajout progressif de fonctionnalités";
-        return "Votre développeur préféré est en plein travail de maintenance sur ce site. Veuillez excuser, les potentiels bugs et l'ajout progressif de fonctionnalités";        ;
+      	//$message ="Le site est pleinement opérationnel ! Dernière nouveauté : Possibilité d'imprimer les cartes des adhérents ;)" 
+      	//$message ="ATTENTION. Afin d'assurer la perpétuitée de ce site internet et de se projet, nous cherchons activement toute idée ou transformation de ce projet par un developpeur interne au lycée afin que le site web continue de satfisfaire un objectif précis.\nSi ce site tombe, faute d'uitlité, le projet entier comprenant la programmation web, serveur et iOS reste ENTIEREMENT disposible sur GitHub via la page ma personnelle.\nLe cas échéant je tiens à assurer que ce projet m'a beaucoup apporté et qu'il a été réalisé avec grand plaisir.\nLe developpeur. 
+      return "<br/>✗✗✗✗✗✗✗--DEVELOPER'S MESSAGE--✗✗✗✗✗✗✗<br/><br/><br/>ATTENTION. Afin d'assurer la perpétuitée de ce site internet et de ce projet, nous cherchons activement toute idée ou transformation de ce projet par un developpeur interne au lycée afin que le site web continue de satfisfaire un objectif précis.<br /><br />Si ce site tombe, faute d'utilité, le projet entier comprenant la programmation web, serveur et iOS reste ENTIEREMENT disponible sur GitHub via ma <a href=\"http://nathanstchepinsky--nathans1.repl.co/\"> page personnelle</a>.<br /><br />Le cas échéant je tiens à assurer que ce projet m'a beaucoup apporté et qu'il a été réalisé avec grand plaisir. C'est donc pour cette raion qu'avec beaucoup d'émotions et de tristesse, il sera éventuellement bientôt affiché un message de fermeture proche de ce site web et des différentes technologies associées<br /> <br/>ATTENTION : Fermeture des technologies associées au serveur de l'A2L prévue pour le 10/06/2020 00:00 <br/> <br/>Le developpeur <3. <br/><br/><br />✗✗✗✗✗✗✗--DEVELOPER'S MESSAGE--✗✗✗✗✗✗✗<br/>";
     }
 
     function getColor(){
-        return "rgba(247, 0,0, 0.7)"; // red=rgba(247, 0, 0,0.5), orange =rgba(240, 145, 4, 0.5)
+        return "rgba(247, 0, 0,0.5)"; // red=rgba(247, 0, 0,0.5), orange =rgba(240, 145, 4, 0.5), vert : rgb(4, 252, 0)
     }
 }
 
